938a93db2feab887605575a0b5fa2cce584d0913
Aanya Kungwani <aanyak820@gmail.com>
2025-07-14 10:00:09 -0400

    Initial Commit

---
+++ To do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To do list
+//
+//  Created by aanya kungwani on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
